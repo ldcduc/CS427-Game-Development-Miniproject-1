ff58174524d2caa3ae0a459be89f67ed1c36cd4d
duc.ldc <duc.ldc@teko.vn>
2019-07-15 13:36:27 +0700

    Finished all animation for Flatboy and Cutegirl

---
+++ Project/Assets/Flatboy/FlatboyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlatboyMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
