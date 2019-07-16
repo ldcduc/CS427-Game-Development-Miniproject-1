e5b5ac28162a3aa87ea29909a22ec1226e59367e
duc.ldc <duc.ldc@teko.vn>
2019-07-16 07:21:29 +0700

    Coding Player movement controller

---
+++ Project/Assets/Flatboy/BoyGrounded.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoyGrounded : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.tag == "Ground") {
+            Player.GetComponent<FlatboyMovement>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.collider.tag == "Ground") {
+            Player.GetComponent<FlatboyMovement>().isGrounded = false;
+        }
+    }
+}
