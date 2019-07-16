6ab4ba016c4b8d9dd0588c06796a6e6fb5004959
duc.ldc <duc.ldc@teko.vn>
2019-07-16 07:21:29 +0700

    Coding Player movement controller

---
+++ Project/Assets/Cutegirl/CutegirlMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutegirlMovement : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    public float moveSpeed = 1f;
+    public float jumpSpeed = 4f;
+
+    float dirX = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(dirX, rb.velocity.y);
+    }
+}
--- Project/Assets/Flatboy/FlatboyMovement.cs
+++ Project/Assets/Flatboy/FlatboyMovement.cs
@@ -4,23 +4,75 @@ using UnityEngine;
 
 public class FlatboyMovement : MonoBehaviour
 {
-    public float speed = 1.0f;
+    private Animator anim;
+    private Rigidbody2D rb;
+    public float moveSpeed = 1f;
+    public float jumpSpeed = 4f;
+    public bool isGrounded = false;
+    public bool isJumping = false;
+    public bool isRunning = false;
+    public bool isWalking = false;
+    const float runSpeed = 3f;
+    const float walkSpeed = 1f;
+    float dirX = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("BoyJump")) {
+            anim.SetBool("isJumping", true);
+            rb.AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) 
+            moveSpeed = walkSpeed;
+        else moveSpeed = runSpeed;
+
         if (Input.GetKey(KeyCode.LeftArrow)) {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
             GetComponent<SpriteRenderer>().flipX = true;
         }
         if (Input.GetKey(KeyCode.RightArrow)) {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
             GetComponent<SpriteRenderer>().flipX = false;
         }
+
+        SetAnimationState();
+
+        if (!anim.GetBool("isDead")) 
+            dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(dirX, rb.velocity.y);
+    }
+
+    void SetAnimationState() {
+        if (isGrounded == true) {
+            anim.SetBool("isJumping", false);
+        }
+
+        if (Mathf.Abs(dirX) == walkSpeed && anim.GetBool("isJumping") == false)
+            anim.SetBool("isWalking", true);
+        else 
+            anim.SetBool("isWalking", false);
+        if (Mathf.Abs(dirX) == runSpeed && anim.GetBool("isJumping") == false) 
+            anim.SetBool("isRunning", true);
+        else 
+            anim.SetBool("isRunning", false);
+
+        // Debug.Log(isGrounded + " " + " isJumping:" + anim.GetBool("isJumping"));
+        // Debug.Log("isWalking:" + anim.GetBool("isWalking"));
+        // Debug.Log("isRunning:" + anim.GetBool("isRunning"));
+        Debug.Log("isJumping:" + anim.GetBool("isJumping"));
+        Debug.Log("isWalking:" + anim.GetBool("isWalking"));
+        Debug.Log("isRunning:" + anim.GetBool("isRunning"));
+        isRunning = anim.GetBool("isRunning");
+        isJumping = anim.GetBool("isJumping");
+        isWalking = anim.GetBool("isWalking");
     }
 }
